75f90668ce67ad678d2ca899cc72e3a8e498cc3c
Christiana Vandermale <christianavdm@gmail.com>
2018-07-21 01:32:48 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+  "My name is XT"
+end
+
+get '/hometown' do
+  "I am from Los Angeles and nowhere."
+end
+
+get '/favorite-song' do
+  "My favorite song is No Horse."
+end
 end
